3255b887e8b6fbb3198ddd920fa92e5832be681b
no4good <45051237+no4good@users.noreply.github.com>
2021-05-02 20:56:56 +0300

    remove fetch

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const fetch = require('node-fetch');
 const { uri, options } = require('./data/config/db.config');
 const errorHandlerMiddleware = require('./api/middlewares/error-handler.middleware');
 const routes = require('./api/routes/index');
